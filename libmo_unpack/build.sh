7f2a6cd37b677b5ec64b6889a263fba6788c3121
Phil Elson <pelson.pub@gmail.com>
2014-08-12 14:42:37 +0100

    Reverted the version of libmo_unpack.

--- libmo_unpack/build.sh
+++ libmo_unpack/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 cd libmo_unpack
-./make_library
-./distribute.sh $PREFIX
-#gcc -c -fPIC -O4 -mfpmath=sse -msse -I include -D_LARGEFILE_SOURCE *.c
-#gcc -shared -Wl,-soname,libmo_unpack.so -o lib/libmo_unpack.so *.o
-#cp lib/* $PREFIX/lib
-#cp include/* $PREFIX/include
+
+gcc -c -fPIC -O4 -mfpmath=sse -msse -I include -D_LARGEFILE_SOURCE *.c
+gcc -shared -Wl,-soname,libmo_unpack.so -o lib/libmo_unpack.so *.o
+
+# Actually install the built product.
+mkdir $PREFIX/lib $PREFIX/include
+cp lib/* $PREFIX/lib
+cp include/* $PREFIX/include
