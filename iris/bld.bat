9cbb740059c37a811477cfedd4ab51d73d3b282a
Phil Elson <pelson.pub@gmail.com>
2015-01-19 15:53:56 +0000

    Fixed cmake Release build for udunits and expat.

--- iris/bld.bat
+++ iris/bld.bat
@@ -1,5 +1,5 @@
 set SITECFG=lib/iris/etc/site.cfg
 echo [System] > %SITECFG%
-echo udunits2_path = %LIBRARY_BIN%\udunits2.dll >> %SITECFG%
+echo udunits2_path = %SCRIPTS%\udunits2.dll >> %SITECFG%
 
 %PYTHON% -sE setup.py install --single-version-externally-managed --record record.txt
