d80695eacf7c36c6481b5360295b43dd12c085f7
marqh <markh@metarelate.net>
2016-12-22 13:39:20 +0000

    pin conda-build to 2.0.6 (#220)
    * pin conda-build to 2.0.6
    
    * pin to conda-build 2.0.10

--- scripts/run_docker_build.sh
+++ scripts/run_docker_build.sh
@@ -42,8 +42,8 @@ conda config --add channels scitools
 conda config --set show_channel_urls True
 
 # Update both conda-build-all and conda-build to get latest "numpy x.x" specification support.
-conda install --yes --quiet -c conda-forge conda-build-all
-conda update --yes --force conda conda-build
+conda install --yes --quiet -c conda-forge conda-build-all conda-build==2.0.10
+conda update --yes --force conda
 conda install -n root --yes --quiet jinja2 anaconda-client
 
 # A lock sometimes occurs with incomplete builds. The lock file is stored in build_artefacts.
