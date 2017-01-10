c97241f017441407fe952f698f7a383eedfd1933
Peter Killick <peter.killick@metoffice.gov.uk>
2017-01-10 11:36:58 +0000

    Use SHLIB_EXT  in build

--- iris/build.sh
+++ iris/build.sh
@@ -3,15 +3,10 @@
 # Make sure Iris can find the udunits library while it's in the temporary
 # build environment. This is necessary because the build process also compiles
 # the pyke rules, which requires importing iris.
-if [[ $(uname) == Darwin ]]; then
-    EXT=dylib
-else
-    EXT=so
-fi
 
 SITECFG=lib/iris/etc/site.cfg
 echo "[System]" > $SITECFG
-echo "udunits2_path = $PREFIX/lib/libudunits2.${EXT}" >> $SITECFG 
+echo "udunits2_path = $PREFIX/lib/libudunits2${SHLIB_EXT}" >> $SITECFG 
 
 rm -rf lib/iris/tests/results lib/iris/tests/*.npz
 
